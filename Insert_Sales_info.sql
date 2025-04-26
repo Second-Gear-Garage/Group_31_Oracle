84a582dd650963bb0c2520898d5abe3c09985554
Dragonfly123-DataScience <marlienaude@gmail.com>
2025-04-26 20:06:00 +0200

    Insert Sales info

---
+++ Insert_Sales_info.sql
@@ -0,0 +1,21 @@
+-- Create the sequence for generating Invoice_IDs
+-- Insert sample records
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (1, 1, 6);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (2, 2, 2);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (3, 3, 10);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (4, 4, 3);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (5, 5, 8);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (6, 6, 1);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (7, 1, 4);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (8, 2, 7);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (9, 3, 5);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (10, 4, 9);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (5, 5, 6);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (9, 6, 2);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (3, 1, 12);
+INSERT INTO PART_SALE_INFO (Invoice_ID, Part_ID, Quantity) VALUES (2, 2, 3);
+
+COMMIT;
+
+
+
